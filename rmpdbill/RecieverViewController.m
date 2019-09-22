3d423e293f7b68d32527eccf69d4cb5b5ad65f95
Rodrigo Andrade <rsa7@rice.edu>
2019-09-21 20:51:35 -0500

    QR Code scanner now works

--- rmpdbill/RecieverViewController.m
+++ rmpdbill/RecieverViewController.m
@@ -11,7 +11,9 @@
 #import "ZXingObjC.h"
 
 @interface RecieverViewController ()
-@property (weak, nonatomic) IBOutlet UIImageView *testQR;
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
 
 @end
 
@@ -19,7 +21,101 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.testQR setImage:self.imageToDecode];
+    [self setupTextFields];
+    
+}
+
+- (void)setupTextFields{
+    
+    ZXLuminanceSource *source = [[ZXCGImageLuminanceSource alloc] initWithCGImage:[self.imageToDecode CGImage]];
+    ZXBinaryBitmap *bitmap = [ZXBinaryBitmap binaryBitmapWithBinarizer:[ZXHybridBinarizer binarizerWithSource:source]];
+    
+    NSError *error = nil;
+    
+    // There are a number of hints we can give to the reader, including
+    // possible formats, allowed lengths, and the string encoding.
+    ZXDecodeHints *hints = [ZXDecodeHints hints];
+    
+    ZXMultiFormatReader *reader = [ZXMultiFormatReader reader];
+    ZXResult *result = [reader decode:bitmap
+                                hints:hints
+                                error:&error];
+    if (result) {
+        // The coded result as a string. The raw data can be accessed with
+        // result.rawBytes and result.length.
+        NSString *contents = result.text;
+        
+        // The barcode format, such as a QR code or UPC-A
+        ZXBarcodeFormat format = result.barcodeFormat;
+        
+        NSString *display = [NSString stringWithFormat:@"%@", contents];
+        
+        NSArray *infoArray = [display componentsSeparatedByString:@";"];
+        NSLog(@"Info Array is %@", infoArray);
+        
+        self.nameTextField.text = [infoArray[0] componentsSeparatedByString:@","][0];
+        self.emailTextField.text = [infoArray[3] componentsSeparatedByString:@"'"][1];
+        self.phoneTextField.text = [infoArray[10] componentsSeparatedByString:@"'"][1];
+        
+    } else {
+        // Use error to determine why we didn't get a result, such as a barcode
+        // not being found, an invalid checksum, or a format inconsistency.
+    }
+}
+
+- (NSString *)barcodeFormatToString:(ZXBarcodeFormat)format {
+    switch (format) {
+        case kBarcodeFormatAztec:
+            return @"Aztec";
+            
+        case kBarcodeFormatCodabar:
+            return @"CODABAR";
+            
+        case kBarcodeFormatCode39:
+            return @"Code 39";
+            
+        case kBarcodeFormatCode93:
+            return @"Code 93";
+            
+        case kBarcodeFormatCode128:
+            return @"Code 128";
+            
+        case kBarcodeFormatDataMatrix:
+            return @"Data Matrix";
+            
+        case kBarcodeFormatEan8:
+            return @"EAN-8";
+            
+        case kBarcodeFormatEan13:
+            return @"EAN-13";
+            
+        case kBarcodeFormatITF:
+            return @"ITF";
+            
+        case kBarcodeFormatPDF417:
+            return @"PDF417";
+            
+        case kBarcodeFormatQRCode:
+            return @"QR Code";
+            
+        case kBarcodeFormatRSS14:
+            return @"RSS 14";
+            
+        case kBarcodeFormatRSSExpanded:
+            return @"RSS Expanded";
+            
+        case kBarcodeFormatUPCA:
+            return @"UPCA";
+            
+        case kBarcodeFormatUPCE:
+            return @"UPCE";
+            
+        case kBarcodeFormatUPCEANExtension:
+            return @"UPC/EAN extension";
+            
+        default:
+            return @"Unknown";
+    }
 }
 
 /*
